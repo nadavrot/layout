179b9cd729f8a29cdaf3a1deffa309b6d581aaed
mholt <mholt@pacificbiosciences.com>
2024-06-14 10:32:03 -0700

    adds fill=context-stroke to SVG markers

--- layout/src/backends/svg.rs
+++ layout/src/backends/svg.rs
@@ -12,11 +12,11 @@ static SVG_HEADER: &str =
 static SVG_DEFS: &str = r#"<defs>
 <marker id="startarrow" markerWidth="10" markerHeight="7"
 refX="0" refY="3.5" orient="auto">
-<polygon points="10 0, 10 7, 0 3.5" />
+<polygon points="10 0, 10 7, 0 3.5" fill="context-stroke" />
 </marker>
 <marker id="endarrow" markerWidth="10" markerHeight="7"
 refX="10" refY="3.5" orient="auto">
-<polygon points="0 0, 10 3.5, 0 7" />
+<polygon points="0 0, 10 3.5, 0 7" fill="context-stroke" />
 </marker>
 
 </defs>"#;
